3694dae577fef7fdaf9783030a2ce4c53df9b536
Gerry Pass <rgpass@gmail.com>
2019-10-25 09:21:51 -0400

    Base changes

--- config/index.js
+++ config/index.js
@@ -1,23 +1,23 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Gerry Pass', // Navigation and Site Title
+  siteTitleAlt: 'Gerry Pass', // Alternative Site title for SEO
+  siteTitleManifest: 'GerryPass',
+  siteUrl: 'https://www.gerrypass.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
+  siteHeadline: 'Articles on React, Node, Rails, and Cypress', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'I write articles on React, Node, Rails, and Cypress.', // Your site description
+  author: 'Gerry Pass', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-6',
+  userTwitter: '@rgp4ss', // Twitter Username - Optional
+  // ogSiteName: 'minimal', // Facebook Site Name - Optional
+  // ogLanguage: 'en_US', // Facebook Language
+  // googleAnalyticsID: 'UA-47519312-6',
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
